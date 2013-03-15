a4273c992ffab617bbb8e1bb40b400a68c87fb5d
Evan Coury <me@evancoury.com>
2013-03-15 10:20:55 -0700

    Allow application config to use merged glob config

--- config/autoload/modulelisteneroptions.global.php
+++ config/autoload/modulelisteneroptions.global.php
@@ -1,11 +1,5 @@
 <?php
 return array(
-    // This should be an array of module namespaces used in the application.
-    'modules' => array(
-        'Application',
-    ),
-
-    // These are various options for the listeners attached to the ModuleManager
     'module_listener_options' => array(
         // This should be an array of paths in which modules reside.
         // If a string key is provided, the listener will consider that a module
@@ -20,7 +14,7 @@ return array(
         // modules are loaded. These effectively overide configuration
         // provided by modules themselves. Paths may use GLOB_BRACE notation.
         'config_glob_paths' => array(
-            'config/autoload/{,*.}{global,local}.php',
+            CONFIG_GLOB,
         ),
 
         // Whether or not to enable a configuration cache.
@@ -48,17 +42,4 @@ return array(
         // 'check_dependencies' => true,
     ),
 
-    // Used to create an own service manager. May contain one or more child arrays.
-    //'service_listener_options' => array(
-    //     array(
-    //         'service_manager' => $stringServiceManagerName,
-    //         'config_key'      => $stringConfigKey,
-    //         'interface'       => $stringOptionalInterface,
-    //         'method'          => $stringRequiredMethodName,
-    //     ),
-    // )
-
-   // Initial configuration with which to seed the ServiceManager.
-   // Should be compatible with Zend\ServiceManager\Config.
-   // 'service_manager' => array(),
 );
--- public/index.php
+++ public/index.php
@@ -8,5 +8,10 @@ chdir(dirname(__DIR__));
 // Setup autoloading
 require 'init_autoloader.php';
 
+// Just for Thinkscape
+define('CONFIG_GLOB', 'config/autoload/{,*.}{global,local}.php');
+$files  = Zend\Stdlib\Glob::glob(CONFIG_GLOB, Zend\Stdlib\Glob::GLOB_BRACE);
+$config = Zend\Config\Factory::fromFiles($files);
+
 // Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+Zend\Mvc\Application::init($config)->run();
